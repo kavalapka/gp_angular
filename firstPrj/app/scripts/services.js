6c78c7bc9e0e82d88483eae8f311a2c3bc286959
kavalapka <zenchik_katia@mail.ru>
2016-02-12 14:08:29 +0300

    Lesson3: fix service DI

--- firstPrj/app/scripts/services.js
+++ firstPrj/app/scripts/services.js
@@ -3,7 +3,7 @@
 angular.module('gpApp')
 
     .service('currencyService', ['$resource', function($resource) {
-        var baseURL = 'http://api.fixer.io/latest'
+        var baseURL = 'http://api.fixer.io/latest';
         this.getCurrency = function() {
             return $resource(baseURL).get();
         };
